1700df1fb4263fdf08453e8ac2af2be92072ad4d
notzuki <101867510+notzuki@users.noreply.github.com>
2025-08-06 20:17:02 +0300

    task 2 done 🐲

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -1,7 +1,7 @@
 import iziToast from "izitoast";
 import "izitoast/dist/css/iziToast.min.css";
 
-const form = document.querySelector('.form');
+
 
 form.addEventListener('submit', event => {
     event.preventDefault();
