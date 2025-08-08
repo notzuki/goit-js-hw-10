ad22c83ff354eb3299585388b4a7754cc2bf307a
notzuki <101867510+notzuki@users.noreply.github.com>
2025-08-08 20:21:36 +0300

    snackbar fix ✈️

--- src/js/2-snackbar.js
+++ src/js/2-snackbar.js
@@ -6,7 +6,7 @@ const form = document.querySelector('.form');
 form.addEventListener('submit', event => {
     event.preventDefault();
 
-    const delay = Number(form.elements.delay.value);
+    
     const state = form.elements.state.value;
 
     createPromise(delay, state)
